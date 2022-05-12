5d35f9286f04e08da6dd49ef79b8c975e7b736a4
joanna amanda <joanna.amanda12@hotmail.com>
2022-05-11 21:58:52 -0300

    primeiro commit

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,60 @@
+/// <reference types="Cypress" />
+
+
+
+describe('Cadastro', () => {
+    it('usuario deve se tornar um entregador', () => {
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+
+        var entregador = {
+            nome: 'Joanna Amanda',
+            cpf: '09878765645',
+            email: 'joanna.amanda12@hotmail.com',
+            whatsapp: '81998087262',
+            endereco: {
+                cep: '54470220',
+                rua: 'Rua Sertão',
+                numero: '0',
+                complemento: 'quadra 24a bloco a16 apt 302',
+                bairro: 'Barra de Jangada',
+                cidade_uf: 'Jaboatão dos Guararapes/PE'
+            },
+            metodo_entrega:'Moto',
+            cnh:'cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+
+        cy.contains('.delivery-method li',entregador.metodo_entrega).click()
+
+        cy.get('dropzone input').attachFile(entregador.cnh)
+
+        cy.get('form button[type="submit"]').click()
+        
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        cy.get('.swal2-container .swal2-html-container').should('have.text',expectedMessage)
+
+
+    })
+}
+)
